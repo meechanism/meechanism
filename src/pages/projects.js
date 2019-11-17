c7a2e24ed87b91a6438eccae0095a123cb539d4b
Mee Cha <mee.cha.nism@gmail.com>
2019-11-17 15:50:29 -0800

    Oops, fix projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -8,41 +8,13 @@ import Button from "../components/button"
 
 const Page = props => {
   const { data, location } = props
-  console.log("*******data: ", data)
-  // const posts = data.allMarkdownRemark.edges
   const siteTitle = "Projects"
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title={siteTitle} />
-      {/* {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
-        return (
-          <div key={node.fields.slug}>
-            <h3
-              style={{
-                marginBottom: rhythm(1 / 4),
-              }}
-            >
-              <Link
-                style={{ boxShadow: `none` }}
-                to={`blog${node.fields.slug}`}
-              >
-                {title}
-              </Link>
-            </h3>
-            <small>{node.frontmatter.date}</small>
-            <p
-              dangerouslySetInnerHTML={{
-                __html: node.frontmatter.description || node.excerpt,
-              }}
-            />
-          </div>
-        )
-      })} */}
-      <Link to="/">
-        <Button marginTop="85px">Go Home</Button>
-      </Link>
+      <h1>{siteTitle}</h1>
+      This area is still under construction! Come back soon!
     </Layout>
   )
 }
