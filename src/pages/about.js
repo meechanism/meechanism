9731e108a2ec017e8db005f31702ad5c38b25ff9
Mee Cha <mee.cha.nism@gmail.com>
2020-01-22 17:55:22 -0800

    Fix typo for import

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Pill, PillBox } from "../components/Pill"
+import { Pill, PillBox } from "../components/pill"
 import { rhythm } from "../utils/typography"
 import Colors from "../components/colors"
 
